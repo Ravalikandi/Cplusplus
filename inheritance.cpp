6c5adc0aadc027fa5923d97afb53e9f192586158
Ravalikandi <ravalireddy245@gmail.com>
2024-10-23 13:09:07 +0530

    Create inheritance.cpp

---
+++ inheritance.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Device{
+     protected:
+     string name,location,model;
+    
+};
+class printer{
+    private: string type;
+    
+};
+class scanner{
+
+};
+int main() {
+   scanner();
+   printer();
+}
